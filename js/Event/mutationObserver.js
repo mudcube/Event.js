d314ea45d2d2cb6f0b32f698094337bdc8f13a01
Michael Deal <mudcube@gmail.com>
2013-04-28 14:07:15 -0700

    add: Event.MutationObserver—detects changes on DOM attributes, such as when attributes in element.style have changed
    http://jsfiddle.net/zFVyv/10

---
+++ js/Event/mutationObserver.js
@@ -0,0 +1,44 @@
+if (typeof(Event) === "undefined") var Event = {};
+
+Event.MutationObserver = (function() { // http://jsfiddle.net/zFVyv/10/
+	var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+	var DOMAttrModifiedSupported = (function() {
+		var p = document.createElement("p");
+		var flag = false;
+		var fn = function() { flag = true };
+		if (p.addEventListener) {
+			p.addEventListener("DOMAttrModified", fn, false);
+		} else if (p.attachEvent) {
+			p.attachEvent("onDOMAttrModified", fn);
+		} else {
+			return false;
+		}
+		///
+		p.setAttribute("id", "target");
+		///
+		return flag;
+	})();
+	///
+	return function(container, callback) {
+		if (MutationObserver) {
+			var options = {
+				subtree: false,
+				attributes: true
+			};
+			var observer = new MutationObserver(function(mutations) {
+				mutations.forEach(function(e) {
+					callback.call(e.target, e.attributeName);
+				});
+			});
+			observer.observe(container, options)
+		} else if (DOMAttrModifiedSupported) {
+			Event.add(container, "DOMAttrModified", function(e) {
+				callback.call(container, e.attrName);
+			});
+		} else if ("onpropertychange" in document.body) {
+			Event.add(container, "propertychange", function(e) {
+				callback.call(container, window.event.propertyName);
+			});
+		}
+	}
+})();
